7672e4ea395acdfb2dab4e7aae24c442c01a7016
KevinKelbie <notkelbie@outlook.com>
2020-01-21 18:21:25 +0000

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+use std::thread;
+use std::sync::mpsc;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use ini::Ini;
+
+extern crate crossbeam;
+#[macro_use]
+extern crate crossbeam_channel;
+
+use crossbeam_channel::{bounded, Sender};
+
+mod api;
+mod database;
+mod network;
+
+
 fn main() {
-    println!("Hello, world!");
+    // database::main();
+
+    // let (s, r) = bounded(0);
+    
+    // let apiHandle = thread::spawn(move || {
+    //     api::main(s);
+    // });
+
+    let networkHandle = thread::spawn(move || {
+        network::main();
+    });
+    
+    networkHandle.join();
+    // apiHandle.join();
+
 }
 
-#[cfg(test)]
 mod tests {
+    use std::io::prelude::*;
+    use std::net::TcpStream;
+
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
+    fn connect_to_port() -> std::io::Result<()> {
+        let mut stream = TcpStream::connect("127.0.0.1:9939")?;
+
+        // stream.write(&[1])?;
+        // stream.read(&mut [0; 128])?;
+        Ok(())
     }
 }
