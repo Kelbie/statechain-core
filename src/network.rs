41b259ead10a9ccb2f404cf087bae63a00e5c1ef
KevinKelbie <notkelbie@outlook.com>
2020-01-30 21:03:21 +0000

    fix for infinite loop
    #3

--- src/network.rs
+++ src/network.rs
@@ -1,80 +1,57 @@
 use std::thread;
+use std::env;
 use std::sync::mpsc;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use ini::Ini;
+use sha2::{Sha256, Sha512, Digest};
+use rocksdb::{DB, Options};
+use bitcoin_hashes::{sha256, Hash};
+use bitcoin_hashes::hex::{FromHex, ToHex};
+
 
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TransferMessage {
-    blindedMessage: String,
+    blinded_message: String,
     signature: String,
     receiver: String
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct InitMessage {
-    owner: String
+    receiver: String
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50]; // using 50 byte buffer
-    while match stream.read(&mut data) {
-        Ok(size) => {
+    let mut data = [0 as u8; 500]; // using 50 byte buffer
+    
+    println!("{:?}", stream.read(&mut data));
 
-            let transferMessage = TransferMessage {
-                blindedMessage: String::from("blindedMessage"),
-                signature: String::from("signature"),
-                receiver: String::from("receiver"),
-            };
+    let digest = sha256::Hash::hash(&data[..]);
 
-            // encode struct to vec
-            let encodedTransferMessage = bincode::serialize(&transferMessage).unwrap();
-
-            // println!("{:?}", encodedTransferMessage.as_slice());
-
-            // let decodedTransferMessage: TransferMessage = bincode::deserialize(&encodedTransferMessage[..]).unwrap();
-            
-            // println!("{:?}", decodedTransferMessage);
-
-            stream.write(encodedTransferMessage.as_slice()).unwrap();
-            true
-        },
-        Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
+    // save encoded message with digest as key
+    let args: Vec<String> = env::args().collect();
+    let path = String::from("/Users/kevinkelbie/Documents/GitHub/statechain-core/src/") + args.get(2).unwrap();
+    let db = DB::open_default(path).unwrap();
+    db.put(String::from(digest.to_hex()), &data[..]).unwrap();
+    
 }
 
 pub fn main() {
-    // let conf = Ini::load_from_file("statechain.conf").unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    // let section = conf.section(Some("statechain".to_owned())).unwrap();
-    // let port = section.get("port").unwrap();
-    // let peers = section.get("peers").unwrap();
+    let conf = Ini::load_from_file(args.get(1).unwrap()).unwrap();
 
-    // println!("peers: {:?}", peers);
-
-    let port = "9939";
+    let section = conf.section(Some("network".to_owned())).unwrap();
+    let port = section.get("network.port").unwrap();
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
     
     println!("Server listening on port {}", port);
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
+        handle_client(stream.unwrap());
     }
     drop(listener);
 }
